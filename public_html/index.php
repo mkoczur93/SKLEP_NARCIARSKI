bc69d72ad80b846d6272b33042729b5024f917a9
GrzegorzBartecki <48419511+GrzegorzBartecki@users.noreply.github.com>
2020-11-21 23:38:47 +0100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -87,7 +87,13 @@
       <div class="container">
         
         <h1>  otwarcie sezonu. nowe promocje </h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p>This is a template for a simple marketing or informational website. 
+        It includes a large callout called a jumbotron and three supporting pieces of content. 
+        Use it as a starting point to create something more unique.This is a template for a simple marketing or informational website. 
+        It includes a large callout called a jumbotron and three supporting pieces of content. 
+        Use it as a starting point to create something more uniqueThis is a template for a simple marketing or informational website. 
+        It includes a large callout called a jumbotron and three supporting pieces of content. 
+        Use it as a starting point to create something more unique</p>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
       </div>
     </div>
