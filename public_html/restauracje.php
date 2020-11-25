e091ae1a34ffc9b64a00fe64dd2c0b7cbf5f0dad
mkoczur93 <48414046+mkoczur93@users.noreply.github.com>
2020-11-25 22:28:27 +0100

    Aktualizacja

---
+++ public_html/restauracje.php
@@ -0,0 +1,386 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Serwis narciaski WEB-SKI</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+
+    </head>
+	
+	
+	
+
+	
+	  <header>
+
+	  <div class="container no-gutters">
+      <!-- Example row of columns -->
+      <div class="row no-gutters">
+	 <div class="col-md-12 logo">
+		<img src="img/logo.png" alt="Obrazek_noclegi">	
+		
+	 </div>
+	 </div>
+  </div>
+	
+	 <!--komentarz>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+   
+		 <!--menu glowne-->
+		 <div id="navbar">
+<div class="bg-dark container-fluid menu "> 
+    <div class="container" style="padding-left: 0px;padding-right: 0px;font-size: 12px;">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+  
+    <ul class="nav navbar-nav" >
+      <li><a href="http://localhost/sklep_narciarski/public_html/index.php">Strona Główna</a></li>
+
+<!--Dropp menu-->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Cennik
+        </a>
+        <div class="dropdown-menu dropstyle">
+          <a class="dropdown-item dropstyle"  href="http://localhost/sklep_narciarski/public_html/cennikWypozyczen.php">Cennik wypożyczeń</a>
+          <a class="dropdown-item dropstyle" href="http://localhost/sklep_narciarski/public_html/cennikSprzetu.php">Cennik serwisu sprzętu</a>
+          <a class="dropdown-item dropstyle" href="http://localhost/sklep_narciarski/public_html/cennikLekcji.php">Cennik lekcji z instruktorem</a>
+        </div>
+      </li>
+<!--Dropp end-->
+
+      <li><a href="http://localhost/sklep_narciarski/public_html/kontakt.php">Kontakt</a></li>
+      <li><a href="http://localhost/sklep_narciarski/public_html/sklep.php">Sklep</a></li>
+      <li><a href="http://localhost/sklep_narciarski/public_html/trasy.php">Trasy</a></li>
+	  <li><a href="http://localhost/sklep_narciarski/public_html/instruktorzy.php">Instruktorzy</a></li>
+      <li><a href="http://localhost/sklep_narciarski/public_html/noclegi.php">Noclegi</a></li>
+      <li class="active"><a href="http://localhost/sklep_narciarski/public_html/restauracje.php">Restauracje</a></li>
+      <li><a href="http://localhost/sklep_narciarski/public_html/kameraonline.php">Kamera Online</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="http://localhost/sklep_narciarski/public_html/rejestracja.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="http://localhost/sklep_narciarski/public_html/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+</div>
+</div>
+</div>
+         <!--koniec menu glownego-->
+</header>
+	
+
+	
+    <body>
+	 
+    
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row no-gutters">
+		
+        <div class="col-md-12 nocleg ">		
+		
+          <div class="col-md-4">
+					<figure>
+						<img src="img/restauracje/restauracja1.jpg" alt="Obrazek_restauracji">							
+					</figure>
+		
+		
+		  </div>
+		  <div class="col-md-8">
+					<div class="col-md-12">
+					
+								<p><h3 class="nocleg">Karczma Góralska Wiejska Izba </h1></p>
+					</div>
+					
+				
+					<div class="col-md-12">
+				
+						<p> Godziny otwarcia: 8.00-23.00. </br> 
+							Liczba dań do wyboru 80. </br> 
+							Średnia cena obiadu 18 PLN. </br> 
+							Specjalność to kuchnia: polska, staropolska. </br> 
+							Do dyspozycji gości drink bar.</br> 
+							Adres: ul. Kluczborska 10B, Stare Olesno, Olesno, opolskie </br> 
+							Telefon: 501-182-083
+						
+						
+						</p>
+
+					</div>
+					
+					<div class="col-md-12">
+					<div class="col-md-9">
+					</div>
+					<div class="col-md-3">
+					 <button class="nocleg" onclick="window.location.href='https://meteor-turystyka.pl/wiejskaizba-stare-olesno,stare-olesno.html';">Zobacz więcej</button></p>
+					</div>
+					</div>
+		
+		
+		
+           </div>
+       
+		</div>
+		
+		
+		
+		<!-- Nastepna restauracja -->
+		
+		
+		
+		
+		   <div class="col-md-12 nocleg ">		
+		
+          <div class="col-md-4">
+					<figure>
+						<img src="img/restauracje/restauracja2.jpg" alt="Obrazek_restauracji">						
+					</figure>
+		
+		
+		  </div>
+		  <div class="col-md-8">
+					<div class="col-md-12">
+					
+						   <p><h3 class="nocleg">Restauracja u Kasi </h3></p>
+					</div>
+					
+				
+					<div class="col-md-12">
+				
+						<p> Godziny otwarcia: 8.00-23.00. </br> 
+                            Liczba dań do wyboru 80. </br> 
+                            Średnia cena obiadu 18 PLN. </br> 
+                            Specjalność to kuchnia: polska, staropolska. </br> 
+                            Do dyspozycji gości drink bar.</br> 
+                            Adres: ul. Kluczborska 10B, Stare Olesno, Olesno, opolskie </br> 
+                            Telefon: 501-182-083
+                        
+                        
+                        </p>
+        
+					</div>
+					
+					
+					
+					<div class="col-md-12">
+					<div class="col-md-9">
+					</div>
+					<div class="col-md-3">
+					 <button class="nocleg" onclick="window.location.href='https://meteor-turystyka.pl/mrowce,zakopane.html';">Zobacz więcej</button></p>
+					</div>
+					</div>
+		
+		
+		
+           </div>
+       
+		</div>
+		
+		<!-- Nastepna restauracja -->
+		
+		  <div class="col-md-12 nocleg ">		
+		
+          <div class="col-md-4">
+					<figure>
+						<img src="img/restauracje/restauracja3.jpg" alt="Obrazek_restauracji">							
+					</figure>
+		
+		
+		  </div>
+		  <div class="col-md-8">
+					<div class="col-md-12">
+					
+								<p><h3 class="nocleg">Zajazd Hema </h1></p>
+					</div>
+					
+				
+					<div class="col-md-12">
+				
+						<p> Godziny otwarcia: 8.00-23.00. </br> 
+                            Liczba dań do wyboru 80. </br> 
+                            Średnia cena obiadu 18 PLN. </br> 
+                            Specjalność to kuchnia: polska, staropolska. </br> 
+                            Do dyspozycji gości drink bar.</br> 
+                            Adres: ul. Kluczborska 10B, Stare Olesno, Olesno, opolskie </br> 
+                            Telefon: 501-182-083
+                        
+                        
+                        </p>
+        
+					</div>
+		
+		
+					<div class="col-md-12">
+					<div class="col-md-9">
+					</div>
+					<div class="col-md-3">
+					 <button class="nocleg" onclick="window.location.href='https://meteor-turystyka.pl/hema,pustkow.html ';">Zobacz więcej</button></p>
+					</div>
+					</div>
+		
+		
+		
+           </div>
+       
+		</div>
+		
+		
+		<!-- Nastepna restauracja -->
+		
+		
+		  <div class="col-md-12 nocleg ">		
+		
+          <div class="col-md-4">
+					<figure>
+						<img src="img/restauracje/restauracja4.jpg" alt="Obrazek_restauracji">								
+					</figure>
+		
+		
+		  </div>
+		  <div class="col-md-8">
+					<div class="col-md-12">
+					
+							<p><h3 class="nocleg">Gościniec Kietrz </h1></p>
+					</div>
+					
+				
+					<div class="col-md-12">
+				
+				
+                        <p> Godziny otwarcia: 8.00-23.00. </br> 
+                            Liczba dań do wyboru 80. </br> 
+                            Średnia cena obiadu 18 PLN. </br> 
+                            Specjalność to kuchnia: polska, staropolska. </br> 
+                            Do dyspozycji gości drink bar.</br> 
+                            Adres: ul. Kluczborska 10B, Stare Olesno, Olesno, opolskie </br> 
+                            Telefon: 501-182-083
+                        
+                        
+                        </p>
+        
+					</div>
+		
+		
+					<div class="col-md-12">
+					<div class="col-md-9">
+					</div>
+					<div class="col-md-3">
+					 <button class="nocleg" onclick="window.location.href='https://meteor-turystyka.pl/gosciniec-kietrz,kietrz.html ';">Zobacz więcej</button></p>
+					</div>
+					</div>
+		
+		
+		
+           </div>
+       
+		</div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<!-- Nastepna restauracja -->
+			
+      </div>
+	  </div>
+  </body>
+
+      <footer>
+
+    <div class="container-fluid">
+      <!-- Example row of columns -->
+     <div class="row no-gutters">
+	    
+		<div class="col-md-12 stopka">
+		
+		<div class="col-md-4">
+		 <p>Dane Kontaktowe:</p>
+		 <p>Sklep Narciarski Web-Ski</p>
+         <p>43-233 Zakopane</p>
+		 <p>ul.Strzegoma 32</p>
+		 
+		 </div>
+		 
+		 
+	    <div class="col-md-4">
+		<p>Partnerzy:</p>
+		<p>Skok Narciarski Czarna Dolina</p>
+	  
+	    </div>
+		
+		
+		
+		<div class="col-md-4">
+
+	  
+	    </div>
+		
+		<div class="col-md-12 text-right">
+				<p>Copyright © 2020 Web-Ski</p>
+		</div>
+		</div>
+		
+	  </div>
+	</div>
+	  
+	  
+	  
+	  
+	  
+	  
+      </footer>
+     <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+        <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	
+		<script>
+        window.onscroll = function() {myFunction()};
+
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        function myFunction() {
+          if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+          } else {
+            navbar.classList.remove("sticky");
+          }
+        }
+		</script>
+		
+</html>
